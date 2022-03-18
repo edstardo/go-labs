e7097237782ff4a8445b4e5aa8e8c398d61ff154
Edsarino Tardo <edsarino.tardo@gmail.com>
2022-03-18 13:09:16 +0800

    Added Sum func with type parameters

--- generics/example/main.go
+++ generics/example/main.go
@@ -6,15 +6,25 @@ type Number interface {
 	int | int32 | int64 | float32 | float64
 }
 
-func Average[N Number](nums []N) float64 {
-	sum := 0.0
-	for n := range nums {
-		sum += float64(n)
+func Sum[N Number](nums []N) N {
+	var sum N
+	for _, n := range nums {
+		sum += n
 	}
+
+	return sum
+}
+
+func Average[N Number](nums []N) float64 {
+	sum := float64(Sum(nums))
+
 	return sum / float64(len(nums))
 }
 
 func main() {
-	fmt.Println(Average([]int{2, 1, 4, 5, 3, 7}))
-	fmt.Println(Average([]float64{2, 1, 4, 5, 3, 7}))
+	fmt.Println(Average([]int{1, 2, 3, 4, 5, 6}))
+	fmt.Println(Average([]int32{1, 2, 3, 4, 5, 6}))
+	fmt.Println(Average([]int64{1, 2, 3, 4, 5, 6}))
+	fmt.Println(Average([]float32{1, 2, 3, 4, 5, 6}))
+	fmt.Println(Average([]float64{1, 2, 3, 4, 5, 6}))
 }
